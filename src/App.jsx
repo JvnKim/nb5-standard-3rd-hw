b206d17b1e65c9e5168bba44154b012b1dc8eb62
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-05-20 19:01:44 +0900

    assignment 1

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,21 @@ export default function App() {
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
-
+  const filteredStudents = students.filter((student) => student.age >= 18);
+  const handleClick = (student) => {
+    alert(`Age: ${student.age}, Grade: ${student.grade}`);
+  };
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
         {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
+        {filteredStudents.map((student) => (
+          <li key={student.name} onClick={() => handleClick(student)}>
+            {student.name}
+          </li>
+        ))}
       </ul>
     </div>
   );
